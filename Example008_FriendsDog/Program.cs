28d5b86f07446e3af76c8edbbe73790bb22d6a74
Dmitrii <datitov2009@yandex.ru>
2022-08-17 09:17:29 +0300

    add Example008 friends and dog

---
+++ Example008_FriendsDog/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+int count = 0;
+int distance = 10000;
+int firstFriendSpeed = 1;
+int secondFriendSpeed = 2;
+int dogSpeed = 5;
+int friend= 2;
+
+while(distance > 10)
+{
+    if(friend == 1)
+    {
+        int time = distance / (firstFriendSpeed + dogSpeed);
+        friend = 2;
+        distance = distance - (firstFriendSpeed + secondFriendSpeed) * time;
+        count++;
+    }
+    else
+    {
+        int time = distance / (secondFriendSpeed + dogSpeed);
+        friend = 1;
+        distance = distance - (firstFriendSpeed + secondFriendSpeed) * time;
+        count++;
+    }
+    
+}
+Console.Write("Собака пробежала ");
+Console.Write(count);
+Console.WriteLine(" раз ");
