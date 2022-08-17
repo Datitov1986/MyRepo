25e5adb4bca93a022f084abf17162b24fe06489c
Dmitrii <datitov2009@yandex.ru>
2022-08-17 08:34:09 +0300

    add Example005 If Else

---
+++ Example005_IfElse/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "маша") //Команда ToLower() позволяет игнорировать регистр букв
+{
+    Console.WriteLine("Ура! Это же Маша!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
+
