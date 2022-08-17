c188b581f48a84624ecf6073607a81b4c59d47b0
Dmitrii <datitov2009@yandex.ru>
2022-08-17 08:19:46 +0300

    Добавил рандомные числа

--- Example003_Sum/Program.cs
+++ Example003_Sum/Program.cs
@@ -1,6 +1,12 @@
 ﻿using System;
 
-int numberA = 13;
-int numberB = 7;
+int numberA = new Random().Next(1,10); //new Random().Next() добавляет случайное 
+//число из заданного диапазона включая первое число и второе число -1. В данном примере числа 1,2,3,4....9
+Console.Write("Первое число: ");
+Console.WriteLine(numberA);
+int numberB = new Random().Next(1,10);
+Console.Write("Второе число: ");
+Console.WriteLine(numberB);
 int result = numberA + numberB;
+Console.Write("Результат сложения двух чисел: ");
 Console.WriteLine(result);
