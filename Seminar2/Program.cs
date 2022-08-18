218c1b0dd6e1479deadc67e1e37640847e1c4b6d
Dmitrii <datitov2009@yandex.ru>
2022-08-18 21:42:30 +0300

    create eminar2 folder

---
+++ Seminar2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+Console.Write("Введите число: ");
+int x = Convert.ToInt32(Console.ReadLine());
+
+if(x==3)
+{
+   Console.Write ("Среда"); 
+}
+else
+    if(x==5)
+{
+    Console.Write ("Пятница:)");
+}
+
+    else
+    {
+    Console.Write ("Семь пятниц на неделе:)");
+    }
