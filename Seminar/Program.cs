e774002df07ead70bd840f1bbf228a7987c6d440
Dmitrii <datitov2009@yandex.ru>
2022-08-18 21:30:12 +0300

    create seminar filder

---
+++ Seminar/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+Console.WriteLine("Введите первое число ");
+int x = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число ");
+int y = Convert.ToInt32(Console.ReadLine());
+
+if(x*x==y)
+{
+    Console.Write("Является квадратом второго числа");
+}
+else
+{
+    Console.Write("Не является квадратом первого числа");
+}
