6c9976ec7b32354db2cb8b5ebb8a740e554b511d
Dmitrii <datitov2009@yandex.ru>
2022-08-18 22:06:59 +0300

    added days in programm

--- Seminar2/Program.cs
+++ Seminar2/Program.cs
@@ -2,17 +2,37 @@
 Console.Write("Введите число: ");
 int x = Convert.ToInt32(Console.ReadLine());
 
-if(x==3)
+if(x==1)
 {
-   Console.Write ("Среда"); 
+   Console.Write("Понедельник"); 
 }
-else
-    if(x==5)
+else if(x==2)
 {
-    Console.Write ("Пятница:)");
+    Console.Write("Вторник");
 }
-
-    else
-    {
-    Console.Write ("Семь пятниц на неделе:)");
-    }
+else if(x==3)
+ {
+    Console.Write("Среда");    
+ }
+else if(x==4)
+{
+     Console.Write("Четверг");
+}
+else if(x==5)
+{
+    Console.Write("Пятница");
+}
+else if(x==6)
+{
+    Console.Write("Суббота");
+}
+else if(x==7)
+{
+    Console.Write("Воскресенье");
+}
+else if(x > 7)
+{
+    Console.Write("Куда разбежался?");
+}
+        
+    
