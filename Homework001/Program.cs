985bf627ef637d712df82d5c972b212eeeb229d1
Dmitrii <datitov2009@yandex.ru>
2022-08-19 09:27:39 +0300

    add homework task 1

---
+++ Homework001/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+Console.Write("Введите первое число: ");
+int x = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int y = Convert.ToInt32(Console.ReadLine());
+
+if(x > y)
+{
+    Console.Write("Первое число больше ");
+}
+else if(x==y)
+{
+    Console.Write("Да числа-то одинаковые! ");
+}
+else if(x < y)
+{
+    Console.Write("Второе число больше ");
+}
