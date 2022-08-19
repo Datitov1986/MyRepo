ad52d0706db2656a67cef29a81bb4de40205adb9
Dmitrii <datitov2009@yandex.ru>
2022-08-19 09:49:32 +0300

    add homework002 folder

---
+++ Homework002/Compare3numbers.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+Console.Write("Введите первое число: ");
+int x = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int y = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите третье число: ");
+int z = Convert.ToInt32(Console.ReadLine());
+int max = x;
+
+if(x > max) max = x;
+if(y > max) max = y;
+if(z > max) max = z;
+
+Console.WriteLine("max = " + max);
