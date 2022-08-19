726623a67dda55768159c4717c5d5736d30cd9c9
Dmitrii <datitov2009@yandex.ru>
2022-08-19 12:51:26 +0300

    add homework 004 folder

---
+++ Homework004/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+Console.Write("Введите число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+int x = 0;
+
+while(x < N - 1)
+{
+    x = x + 2;
+    Console.Write(x);
+    Console.Write(", ");
+}
+Console.WriteLine(" как-то так");
