d9f0d96bc8d2071fe563b9a2f60529df85563c4d
Dmitrii <datitov2009@yandex.ru>
2022-08-19 14:02:22 +0300

    add homework optional 002 folder

---
+++ HomeworkOptional002/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+Console.Write("Ведите число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+int x = -N;
+while(x <= N)
+{
+    Console.Write(x);
+    Console.Write(", ");
+    x++;
+}
+Console.WriteLine("Something Like That");
