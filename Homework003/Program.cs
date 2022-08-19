ad02e083bbb5cb602a76680d6116423ac2c4d956
Dmitrii <datitov2009@yandex.ru>
2022-08-19 12:13:41 +0300

    add homework003 folder

---
+++ Homework003/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+Console.Write("Введите число: ");
+int x = Convert.ToInt32(Console.ReadLine());
+
+if(x % 2 == 1)
+{
+    Console.WriteLine("Ваше число нечетное ");
+}
+else
+{
+    Console.WriteLine("Ваше число четное ");
+}
