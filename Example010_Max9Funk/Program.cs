f723f631eb6c5e9048c892b591d59388de925ec8
Dmitrii <datitov2009@yandex.ru>
2022-08-20 12:36:29 +0300

    изменил код, заменил чать его комментарием

--- Example010_Max9Funk/Program.cs
+++ Example010_Max9Funk/Program.cs
@@ -10,15 +10,18 @@ int a1 = 15;
 int b1 = 211;
 int c1 = 39;
 int a2 = 12;
-int b2 = 23;
+int b2 = 22453;
 int c2 = 33;
 int a3 = 13;
 int b3 = 23;
-int c3 = 3334;
+int c3 = 334;
 
-int max1 = Max(a1,b1,c1);
-int max2 = Max(a2,b2,c2);
-int max3 = Max(a3,b3,c3);
-int max = Max(max1, max2, max3);
+//int max1 = Max(a1,b1,c1);
+//int max2 = Max(a2,b2,c2);
+//int max3 = Max(a3,b3,c3);
+int max = Max(
+    Max(a1,b1,c1), 
+    Max(a2,b2,c2), 
+    Max(a3,b3,c3));
 
 Console.WriteLine(max);
