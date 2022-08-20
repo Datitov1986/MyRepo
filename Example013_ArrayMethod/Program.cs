bab026ecbaec6a1f9e449187d445afd6800a6b59
Dmitrii <datitov2009@yandex.ru>
2022-08-20 14:04:51 +0300

    add example 013 array method folder

---
+++ Example013_ArrayMethod/Program.cs
@@ -0,0 +1,54 @@
+﻿using System.Reflection.Emit;
+using System.Data;
+using System.Collections;
+using System.Collections.ObjectModel;
+using System;
+
+void FillArray(int[] collection) // void метод, который не возвращает значения -> не нужно использовать оператор return
+{
+
+    int length = collection.Length;
+    int index = 0;
+    while(index < length)
+    {
+        collection[index] = new Random().Next(1, 10);
+        index++;
+    }
+}
+void PrintArray(int[] col)
+{
+    int count = col.Length;
+    int position = 0;
+    while(position < count)
+    {
+
+        Console.Write(col[position] + ", ");
+        position++;
+    }
+}
+
+int IndexOf(int[] collection, int find)
+{
+    int count = collection.Length;
+    int index = 0;
+    int position = -1;
+    while(index < count)
+    {
+        if(collection[index] == find)
+        {
+            position = index;
+            break;
+        }
+        index++;
+    }
+    return position;
+
+}
+int[] array = new int[10];// new int[10] - Создай новый массив, в котром будет 10 элементов
+
+FillArray(array);
+PrintArray(array);
+Console.WriteLine();
+
+int pos = IndexOf(array, 4);
+Console.WriteLine("Ваша позиция: " + pos);
