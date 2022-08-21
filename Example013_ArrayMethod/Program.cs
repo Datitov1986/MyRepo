e2f1b5cef36902ee45d529a1fcddbd0dd1dbd3da
Dmitrii <datitov2009@yandex.ru>
2022-08-21 19:00:01 +0300

    Немного исправил код, чтобы сворачивался полностью

--- Example013_ArrayMethod/Program.cs
+++ Example013_ArrayMethod/Program.cs
@@ -5,46 +5,46 @@ using System.Collections.ObjectModel;
 using System;
 
 void FillArray(int[] collection) // void метод, который не возвращает значения -> не нужно использовать оператор return
-{
-
-    int length = collection.Length;
-    int index = 0;
-    while(index < length)
     {
-        collection[index] = new Random().Next(1, 10);
-        index++;
+
+        int length = collection.Length;
+        int index = 0;
+        while(index < length)
+        {
+            collection[index] = new Random().Next(1, 10);
+            index++;
+        }
     }
-}
 void PrintArray(int[] col)
-{
-    int count = col.Length;
-    int position = 0;
-    while(position < count)
     {
+        int count = col.Length;
+        int position = 0;
+        while(position < count)
+        {
 
-        Console.Write(col[position] + ", ");
-        position++;
+            Console.Write(col[position] + ", ");
+            position++;
+        }
     }
-}
 
 int IndexOf(int[] collection, int find)
-{
-    int count = collection.Length;
-    int index = 0;
-    int position = -1; // ставлю значение -1 чтобы, в случае если нет элемента удовлетворяющего условию
-    //появлялось значение -1, что будет означать что такой элемент не найден
-    while(index < count)
     {
-        if(collection[index] == find)
+        int count = collection.Length;
+        int index = 0;
+        int position = -1; // ставлю значение -1 чтобы, в случае если нет элемента удовлетворяющего условию
+        //появлялось значение -1, что будет означать что такой элемент не найден
+        while(index < count)
         {
-            position = index;
-            break;
+            if(collection[index] == find)
+            {
+                position = index;
+                break;
+            }
+            index++;
         }
-        index++;
-    }
-    return position;
+        return position;
 
-}
+    }
 int[] array = new int[10];// new int[10] - Создай новый массив, в котром будет 10 элементов
 
 FillArray(array);
