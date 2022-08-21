2aa9001135fc2eda5ceb2b4e3b8190b3f048a02e
Dmitrii <datitov2009@yandex.ru>
2022-08-21 18:58:50 +0300

    Навел немного красоты

--- Example005_IfElse/Program.cs
+++ Example005_IfElse/Program.cs
@@ -4,12 +4,12 @@ Console.Write("Введите имя пользователя: ");
 string username = Console.ReadLine();
 
 if(username.ToLower() == "маша") //Команда ToLower() позволяет игнорировать регистр букв
-{
-    Console.WriteLine("Ура! Это же Маша!");
-}
+    {
+        Console.WriteLine("Ура! Это же Маша!");
+    }
 else
-{
-    Console.Write("Привет, ");
-    Console.WriteLine(username);
-}
+    {
+        Console.Write("Привет, ");
+        Console.WriteLine(username);
+    }
 
