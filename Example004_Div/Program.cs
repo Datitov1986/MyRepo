6382f772339111ac24fd7861df40d0bf9c887d3c
Dmitrii <datitov2009@yandex.ru>
2022-08-21 18:53:57 +0300

    Сделал код короче на строчку

--- Example004_Div/Program.cs
+++ Example004_Div/Program.cs
@@ -3,5 +3,5 @@
 double numberA = 12;
 double numberB = 5;
 double result = numberA / numberB;
-Console.Write("Результат деления двух чисел: ");
-Console.WriteLine(result);
+Console.WriteLine("Результат деления двух чисел: " + result);
+
