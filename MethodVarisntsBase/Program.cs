80f2af9987688224dbd4258960db40c362a96371
Dmitri <datitov2009@yandex.ru>
2022-09-13 21:14:04 +0300

    refactoring

--- MethodVarisntsBase/Program.cs
+++ MethodVarisntsBase/Program.cs
@@ -1,39 +1,39 @@
 ﻿//Вид 1 (функция, которая ничего не принимает и не возвращает)
 
-void Method1()
-{
-    Console.WriteLine("Автор ...");
-}
+// void Method1()
+// {
+//     Console.WriteLine("Автор ...");
+// }
 //Method1();
 
 //Вид 2 (ничего не возвращают, но принимают значения)
 
-void Method2(string msg)
-{
-    Console.WriteLine(msg);
-}
+// void Method2(string msg)
+// {
+//     Console.WriteLine(msg);
+// }
 //Method2("Текст сообщения"); //неименованные аргументы
 
-void Method21(string msg, int count)
-{
-    int i = 0;
-    while (i < count)
-    {
-        Console.WriteLine(msg);
-        i++; //Инкремент
-    }
-}
+// void Method21(string msg, int count)
+// {
+//     int i = 0;
+//     while (i < count)
+//     {
+//         Console.WriteLine(msg);
+//         i++; //Инкремент
+//     }
+// }
 //Method21("Текст", 4);
 //Method21(msg: "Текст",count: 4); //именованные аргументы, не обязательно писать их по порядку Method21(count: 4, msg: "Текст");
 
 //Вид 3 (функция что-то возвращает)
 
-int Method3()
-{
-    return DateTime.Now.Year;
-}
+// int Method3()
+// {
+//     return DateTime.Now.Year;
+// }
 
-int year = Method3();
+// int year = Method3();
 //Console.WriteLine(year);
 
 
