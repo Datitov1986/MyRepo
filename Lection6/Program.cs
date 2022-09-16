e4332d105ed81b355e8f2411c55c9374165af56a
Dmitri <datitov2009@yandex.ru>
2022-09-16 21:03:45 +0300

    программа с лекции 6 с координатами

---
+++ Lection6/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Linq;
+
+string text = "(1,2) (2,3) (4,5) (6,7)"
+               .Replace("(", "") // Заменяет скобку на пустоту
+               .Replace(")", "") // Заменяет скобку на пустоту
+               ;
+Console.WriteLine(text);
+var data = text.Split(" ")
+               .Select(item => item.Split(',')) // Разделяет символы между которыми стоит запятая
+               .Select(e => (x: int.Parse(e[0]),y: int.Parse(e[1])))
+               .Where(e => e.x % 2 == 0) // будет выводить те точки, у которых координата четная
+               .Select(point => (point.x * 10, point.y))
+               .ToArray();
+
+for (int i = 0; i < data.Length; i++)
+{
+    Console.WriteLine(data[i]);
+    // for (int k = 0; k < data[i].Length; k++)
+    // {
+    //     Console.WriteLine(data[i][k]);
+    // }
+    //Console.WriteLine();
+}
+
